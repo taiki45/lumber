ede41abe3e83ff125d8833ec3765e56202c8d1bf
Taiki ONO <taiks.4559@gmail.com>
2012-04-14 16:06:40 +0900

    Add fizz_buzz file

---
+++ ruby/fizz_buzz.rb
@@ -0,0 +1,25 @@
+def fizz?(num)
+  return true if (num % 3) == 0
+  false
+end
+
+def buzz?(num)
+  return true if (num % 5) == 0
+  false
+end
+
+def fizz_buzz(num)
+  1.upto(num) do |n|
+    if fizz?(n) && buzz?(n)
+      puts 'FizzBuzz'
+    elsif fizz?(n)
+      puts 'Fizz'
+    elsif buzz?(n)
+      puts 'Buzz'
+    else
+      puts n
+    end
+  end
+end
+
+fizz_buzz(30)

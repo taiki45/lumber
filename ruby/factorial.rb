312075f18cc0fde20b9b44588cd04c040ec1c5fb
Taiki ONO <taiks.4559@gmail.com>
2012-04-21 18:22:46 +0900

    Fix indents

--- ruby/factorial.rb
+++ ruby/factorial.rb
@@ -1,10 +1,10 @@
 class Integer
-	def factorial
-		alt = self
-		(alt - 1).downto(1) do |i|
-			alt *= i
-		end
-		alt
-	end
+  def factorial
+    alt = self
+    (alt - 1).downto(1) do |i|
+      alt *= i
+    end
+    alt
+  end
 end
 

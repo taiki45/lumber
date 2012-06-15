ac4553428a421f5653fde82e162860ea086169fb
Taiki ONO <taiks.4559@gmail.com>
2012-06-16 01:15:02 +0900

    fix 'if expression'

--- c/fizz_buzz.c
+++ c/fizz_buzz.c
@@ -6,37 +6,35 @@ int buzz(int num);
 
 int main(int argc, char* argv[])
 {
-    int num, i;
-    if (argc <= 1) {
-        exit(0);
+  int num, i;
+  if (argc <= 1) exit(0);
+  num = atoi(argv[1]);
+  for (i=1; i <= num; i++) {
+    if (fizz(i) && buzz(i)) {
+      printf("FizzBuzz\n");
+    } else if (fizz(i)) {
+      printf("Fizz\n");
+    } else if (buzz(i)) {
+      printf("Buzz\n");
+    } else {
+      printf("%d\n", i);
     }
-    num = atoi(argv[1]);
-    for (i=1; i <= num; i++) {
-        if (fizz(i) && buzz(i)) {
-            printf("FizzBuzz\n");
-        } else if (fizz(i)) {
-            printf("Fizz\n");
-        } else if (buzz(i)) {
-            printf("Buzz\n");
-        } else {
-            printf("%d\n", i);
-        }
-    }
-    return 0;
+  }
+  return 0;
 }
 
 int fizz(int num)
 {
-    if ((num % 3) == 0) {
-        return 1;
-    }
-    return 0;
+  if ((num % 3) == 0) {
+    return 1;
+  }
+  return 0;
 }
 
 int buzz(int num)
 {
-    if ((num % 5) == 0) {
-        return 1;
-    }
-    return 0;
+  if ((num % 5) == 0) {
+    return 1;
+  }
+  return 0;
 }
